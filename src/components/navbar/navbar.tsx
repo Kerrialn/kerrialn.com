7d5abf052600e630f2b7aa68d3427df212287118
Kerrial Newham <kerrialbeckettnewham@gmail.com>
2020-05-02 15:13:06 +0200

    update navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import style from "./navbar.module.css";
 import { Link } from "react-router-dom";
+import logo from "./assests/logo.png";
 
 export const Navbar = () => {
   return (
     <div className={style.navbar}>
+      <img className={style.logo} src={logo} />
       <Link className={style.title} to='/'>
-        Kerrial N
+        <div className={style.titleText}>Kerrial N</div>
       </Link>
       <Link to='/contact'>Contact</Link>
     </div>
