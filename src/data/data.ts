33f6afe63316d692317601877f536b41fec04f5c
Kerrial Newham <kerrialbeckettnewham@gmail.com>
2020-05-02 11:40:04 +0200

    add Easy, cheap, good, working, free and now

--- src/data/data.ts
+++ src/data/data.ts
@@ -7,7 +7,7 @@ export const data = {
       slug: "how-stupid-smart-are-you",
       title: "How stupid smart are you?",
       summary:
-        "wrong assumptions seem to make up the development world and I wonder why.",
+        "I'm tired of some assumptions made by developers (juniour and senior). This issue effects large and small alike. For example, the assumption that if you are in a certain location then you will want to have the application in the local language. Why make this assumption? For what benifit? are you expecting everyone who lives, travels through and visits to speak the local language?",
       content: [
         "I'm tired of some assumptions made by developers (juniour and senior). This issue effects large and small alike. For example, the assumption that if you are in a certain location then you will want to have the application in the local language. Why make this assumption? For what benifit? are you expecting everyone who lives, travels through and visits to speak the local language?",
         "The objective may possibly be to save the user time, but if you inverse that arguement and think about how many people have been inconvenienced, by having to hunt around in the application settings attempting to change the language back to the language they previously selected. My point here is why not just let the user choose? If they want to change language they will.",
@@ -26,11 +26,20 @@ export const data = {
       slug: "easy-cheap-good-working-free-and-now",
       title: "Easy, cheap, good, working, free and now!",
       summary:
-        "Our consumer demands are insane when it comes to technology and under valued",
-      content: [],
+        "It seems to me that the wave of free usage platforms with an advert based profit model, has done far more harm than good. Look at the nearly endless array of awesome and powerful, free tools that we have at our disposal as developers today from github to analytics tools, netlify to cloudflare.",
+      content: [
+        "It seems to me that the wave of free usage platforms with an advert based profit model, has done far more harm than good. Look at the nearly endless array of awesome and powerful, free tools that we have at our disposal as developers today from github to analytics tools, netlify to cloudflare.",
+        "Even if you look at the free stuff available if you're not a developer. Facebook, Google (most products), Coursera, Wix, 1 month free subscriptions for a plethora of services. ",
+        "All these free tools have done is devalue programming in general and raise barriers to entry into the online marketplace. ",
+        "Devaluation has occurred because we have been given so many programeticlly very complex tools for free, therefore complex functionality has become automatically expected by customers, furthermore for free. Even though it may have taken teams of developers many hours to build said functionality. This naturally raises barriers to entry, because smaller tech companies can not afford to hire enough programmers to compete effectively.",
+        "Essentially, the advert based business models have won hands down. But at what price? Well put it like this advertising is very lucrative, but so are data sets, maybe more so.",
+        "Currently users data is the digital currency of the day, the value of data changes based on it type and it’s verifiability. (*Forbes - How Much Is Your Data Worth?) For example knowing a customer's address and having that information verified by a government issued document would make that user's data far more valuable, than just knowing an email address that isn’t verified at all, I think for obvious reasons.  ",
+        "To me the next gold mine is opinion mining, seeing as most economies are built on confidence of the consumer and investors, then knowing customers opinions of your products would be extremely valuable, apply these option data sets to AI pattern recognition algorithms and boom you might have percentage of predictability in public reception or even consumption of a new product. This is just the tip of the iceberg and it’s happening now. ",
+        "The future is one of big data and pattern finding AI, all in an attempt to predict consumer behaviour and reduce company risk. It’ll be good, high quality, cheap or free. Until the accuracy of AI prediction becomes guaranteeable and new data is no longer needed. Then what? ",
+      ],
       sources: [],
-      date: new Date("April 30, 2020 21:25:00"),
-      published: false,
+      date: new Date("May 2, 2020 11:37:00"),
+      published: true,
     },
   ],
 };
